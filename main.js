b5f8e9c401620effdc51cbbe6f55e83b09a77ff9
Shaina Couch <shainacouch21@gmail.com>
2023-02-10 10:49:45 -0600

    old javascrit project

---
+++ main.js
@@ -0,0 +1,15 @@
+const dishes = [
+    "Dinner plate", "Water glass", "Salad bowl",
+    "Dinner plate", "Wine glass", "Spoon",
+    "Spoon", "Fork", "Salad bowl", "Whiskey glass",
+    "Fork", "Spoon", "Fork"
+]
+
+let dirty1 = dishes[3];
+let dirty2 = dishes[8];
+let dirty3 = dishes[0];
+
+console.log("I am cleaning the following glasses:")
+console.log(dirty1);
+console.log(dirty2);
+console.log(dirty3);
